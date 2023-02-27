7a4cac205b37d9b41006442161d7ebae989d4efa
simon <simon.schoeggler@hotmail.com>
2023-02-27 12:42:40 +0100

    Initial commit

---
+++ src/main/java/at/kaindorf/ConcretePublisher.java
@@ -0,0 +1,30 @@
+package at.kaindorf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3>Created by IntelliJ IDEA.</h3><br>
+ * <b>Project:</b> Newspaper-Observer<br>
+ * <b>User:</b> Simon Schoeggler<br>
+ * <b>Date:</b> 27. Februar 2023<br>
+ * <b>Time:</b> 12:21<br>
+ */
+
+public class KleineZeitung implements Publisher {
+    private List<Reader> readers = new ArrayList<>();
+    @Override
+    public void register(Reader reader) {
+        readers.add(reader);
+    }
+
+    @Override
+    public void unregister(Reader reader) {
+        readers.remove(reader);
+    }
+
+    @Override
+    public void notifyReaders() {
+
+    }
+}
